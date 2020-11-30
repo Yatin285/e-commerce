7b8cc4053f9935864d9d79c8ca6e6f8c0554fd34
Yatin285 <72808215+Yatin285@users.noreply.github.com>
2020-11-30 15:02:45 +0530

    Add files via upload

---
+++ websitejs.js
@@ -0,0 +1,94 @@
+function onc1(){
+
+
+var x=document.getElementById("w2");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+}
+
+function onc2(){
+
+var x=document.getElementById("w3");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc3(){
+
+var x=document.getElementById("w4");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc4(){
+
+var x=document.getElementById("w5");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc5(){
+
+var x=document.getElementById("w6");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc6(){
+
+var x=document.getElementById("w7");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc7(){
+
+var x=document.getElementById("w8");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc8(){
+
+var x=document.getElementById("w9");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc9(){
+
+var x=document.getElementById("w10");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc10(){
+
+var x=document.getElementById("w11");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc11(){
+
+var x=document.getElementById("w12");
+	setTimeout(function(){x.innerHTML="1 item added to cart!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+function onc12(){
+
+var x=document.getElementById("w13");
+	setTimeout(function(){x.innerHTML="Thank you for liking!";},1050);
+	setTimeout(function(){x.innerHTML=" ";},5000);
+
+}
+
+function onc13(){
+document.getElementById("bb5").innerHTML="SUBSCRIBED";
+document.getElementById("input1").value=" ";
+}
+function onc14(){
+document.getElementById("bun2").innerHTML="Thank you!";
+document.getElementById("form1").reset();
+}
